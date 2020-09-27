82c08bb2c73f886db7620da539ecb356fbaf3732
gcbiotec <gcbiotec@gmail.com>
2020-09-27 11:22:40 -0300

    Os testes do CachorroController e do VeterinarioController estão funcionando.

--- src/test/java/br/com/fundatec/microservicecourse/controller/VeterinarioControllerTest.java
+++ src/test/java/br/com/fundatec/microservicecourse/controller/VeterinarioControllerTest.java
@@ -1,6 +1,7 @@
 package br.com.fundatec.microservicecourse.controller;
 
 import br.com.fundatec.microservicecourse.domain.Cachorro;
+import br.com.fundatec.microservicecourse.domain.Veterinario;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -18,42 +18,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CachorroControllerTest {
-
-    @Autowired
+public class VeterinarioControllerTest {
+	
+	@Autowired
     private MockMvc mockMvc;
-
+	
+	@Test
+	public void retornaTodosVeterinariosShouldReturnOk() throws Exception{
+		mockMvc
+			.perform(MockMvcRequestBuilders.get("/veterinarios"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk());
+	}
     @Test
-    public void retornaTodosCachorrosShouldReturnOk() throws Exception {
-        mockMvc
-                .perform(MockMvcRequestBuilders.get("/cachorros"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
+    public void retornaPostVeterinarioOK() throws Exception {
 
-    @Test
-    public void retornaPostCachorroOK() throws Exception {
-
-        Cachorro cachorro = new Cachorro(null, "Spike", 15, null, 0);
+        Veterinario veterinario = new Veterinario(null, "Marvin", "11/07/2001", "3237467364", null);
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(cachorro);
+        String json = mapper.writeValueAsString(veterinario);
 
         mockMvc
                 .perform(
-                        MockMvcRequestBuilders.post("/cachorros")
+                        MockMvcRequestBuilders.post("/veterinarios")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(json))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk());
 
+
         mockMvc
-                .perform(
-                        MockMvcRequestBuilders.get("/cachorros/{id}", 1)
+                .perform(MockMvcRequestBuilders.get("/veterinarios/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(jsonPath("$.nome").value("Spike"))
-                .andExpect(jsonPath("$.idade").value(15))
+
+                .andExpect(jsonPath("$.nome").value("Marvin"))
+                .andExpect(jsonPath("$.dataNascimento").value("11/07/2001"))
                 .andExpect(status().isOk());
+
     }
 }
